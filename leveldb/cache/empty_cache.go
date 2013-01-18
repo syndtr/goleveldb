f192f08373b413813d10a13b79892113cb5bdb05
Suryandaru Triandana <syndtr@gmail.com>
2013-01-18 07:48:35 +0700

    cache: introduce EmptyCache

---
+++ leveldb/cache/empty_cache.go
@@ -0,0 +1,65 @@
+// Copyright (c) 2013, Suryandaru Triandana <syndtr@gmail.com>
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This LevelDB Go implementation is based on LevelDB C++ implementation.
+// Which contains the following header:
+//   Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+//   Use of this source code is governed by a BSD-style license that can be
+//   found in the LEVELDBCPP_LICENSE file. See the LEVELDBCPP_AUTHORS file
+//   for names of contributors.
+
+package cache
+
+type EmptyCache struct{}
+
+func (EmptyCache) GetNamespace(id uint64) Namespace {
+	return emptyCacheNs{}
+}
+
+func (EmptyCache) Purge(fin func()) {
+	if fin != nil {
+		fin()
+	}
+}
+
+type emptyCacheNs struct{}
+
+func (emptyCacheNs) Get(key uint64, setf SetFunc) (obj Object, ok bool) {
+	ok, value, _, fin := setf()
+	if !ok {
+		return
+	}
+	obj = &emptyCacheObj{value, fin}
+	return
+}
+
+func (emptyCacheNs) Delete(key uint64, fin func()) bool {
+	if fin != nil {
+		fin()
+	}
+	return false
+}
+
+func (emptyCacheNs) Purge(fin func()) {
+	if fin != nil {
+		fin()
+	}
+}
+
+type emptyCacheObj struct {
+	value interface{}
+	fin   func()
+}
+
+func (p *emptyCacheObj) Release() {
+	if p.fin != nil {
+		p.fin()
+	}
+}
+
+func (p *emptyCacheObj) Value() interface{} {
+	return p.value
+}
