syndtr/goleveldb

commit 960ff1f37b7bf3d9f83e41ea2b949ca043652d49

leveldb: version: Remove use of SetFinalizer around internal types
Related to issue #25

leveldb/db_write.go
@@ -47,7 +47,6 @@ func (d *DB) flush() (m *memdb.DB, err error) {
 		case v.tLen(0) >= kL0_SlowdownWritesTrigger && !delayed:
 			delayed = true
 			time.Sleep(time.Millisecond)
-			continue
 		case mem.cur.Size() <= s.o.GetWriteBuffer():
 			// still room
 			return mem.cur, nil
@@ -62,23 +61,22 @@ func (d *DB) flush() (m *memdb.DB, err error) {
 				cwait = true
 				d.cch <- cWait
 			}
-			continue
 		case v.tLen(0) >= kL0_StopWritesTrigger:
 			d.cch <- cSched
-			continue
-		}
-
-		// create new memdb and journal
-		m, err = d.newMem()
-		if err != nil {
-			return
-		}
-
-		// schedule compaction
-		select {
-		case d.cch <- cSched:
 		default:
+			// create new memdb and journal
+			m, err = d.newMem()
+			if err != nil {
+				return
+			}
+
+			// schedule compaction
+			select {
+			case d.cch <- cSched:
+			default:
+			}
 		}
+		v.release()
 	}
 
 	return

leveldb/db.go
@@ -350,7 +350,9 @@ func (d *DB) get(key []byte, seq uint64, ro *opt.ReadOptions) (value []byte, err
 		return
 	}
 
-	value, cState, err := s.version().get(ikey, ro)
+	v := s.version()
+	value, cState, err := v.get(ikey, ro)
+	v.release()
 	if cState && !d.isClosed() {
 		// schedule compaction
 		select {
@@ -430,7 +432,11 @@ func (d *DB) GetProperty(prop string) (value string, err error) {
 
 	p := prop[len(prefix):]
 
-	switch s := d.s; true {
+	s := d.s
+	v := s.version()
+	defer v.release()
+
+	switch {
 	case strings.HasPrefix(p, "num-files-at-level"):
 		var level uint
 		var rest string
@@ -438,10 +444,9 @@ func (d *DB) GetProperty(prop string) (value string, err error) {
 		if n != 1 || level >= kNumLevels {
 			err = errors.ErrInvalid("invalid property: " + prop)
 		} else {
-			value = fmt.Sprint(s.version().tLen(int(level)))
+			value = fmt.Sprint(v.tLen(int(level)))
 		}
 	case p == "stats":
-		v := s.version()
 		value = "Compactions\n" +
 			" Level |   Tables   |    Size(MB)   |    Time(sec)  |    Read(MB)   |   Write(MB)\n" +
 			"-------+------------+---------------+---------------+---------------+---------------\n"
@@ -455,7 +460,6 @@ func (d *DB) GetProperty(prop string) (value string, err error) {
 				float64(read)/1048576.0, float64(write)/1048576.0)
 		}
 	case p == "sstables":
-		v := s.version()
 		for level, tt := range v.tables {
 			value += fmt.Sprintf("--- level %d ---\n", level)
 			for _, t := range tt {
@@ -482,6 +486,8 @@ func (d *DB) GetApproximateSizes(rr []Range) (Sizes, error) {
 	}
 
 	v := d.s.version()
+	defer v.release()
+
 	sizes := make(Sizes, 0, len(rr))
 	for _, r := range rr {
 		min := newIKey(r.Start, kMaxSeq, tSeek)

leveldb/db_compaction.go
@@ -90,7 +90,7 @@ func (c *cMem) flush(mem *memdb.DB, level int) error {
 	}
 
 	if level < 0 {
-		level = s.version().pickLevel(t.min.ukey(), t.max.ukey())
+		level = s.version_NB().pickLevel(t.min.ukey(), t.max.ukey())
 	}
 	c.rec.addTableFile(level, t)
 
@@ -435,7 +435,7 @@ func (d *DB) compaction() {
 					d.doCompaction(c, true)
 				}
 			} else {
-				v := s.version()
+				v := s.version_NB()
 				maxLevel := 1
 				for i, tt := range v.tables[1:] {
 					if tt.isOverlaps(creq.min, creq.max, true, s.cmp) {
@@ -455,7 +455,7 @@ func (d *DB) compaction() {
 		for {
 			if mem := d.getFrozenMem(); mem != nil {
 				d.memCompaction(mem)
-			} else if s.version().needCompaction() {
+			} else if s.version_NB().needCompaction() {
 				d.doCompaction(s.pickCompaction(), false)
 			} else {
 				break

leveldb/db_iter.go
@@ -22,6 +22,7 @@ func (d *DB) newRawIterator(ro *opt.ReadOptions) iterator.Iterator {
 
 	mem := d.getMem()
 	v := s.version()
+	defer v.release()
 
 	ti := v.getIterators(ro)
 	ii := make([]iterator.Iterator, 0, len(ti)+2)

leveldb/db_test.go
@@ -155,6 +155,7 @@ func (h *dbHarness) maxNextLevelOverlappingBytes(want uint64) {
 			}
 		}
 	}
+	v.release()
 
 	if res > want {
 		t.Errorf("next level overlapping bytes is more than %d, got=%d", want, res)
@@ -393,6 +394,7 @@ func (h *dbHarness) tablesPerLevel(want string) {
 			nz = len(res)
 		}
 	}
+	v.release()
 	res = res[:nz]
 	if res != want {
 		h.t.Errorf("invalid tables len, want=%s, got=%s", want, res)
@@ -404,6 +406,7 @@ func (h *dbHarness) totalTables() (n int) {
 	for _, tt := range v.tables {
 		n += len(tt)
 	}
+	v.release()
 	return
 }
 
@@ -589,7 +592,9 @@ func TestDb_GetLevel0Ordering(t *testing.T) {
 		h.getVal("foo", "v3")
 		h.getVal("bar", "b3")
 
-		t0len := h.db.s.version().tLen(0)
+		v := h.db.s.version()
+		t0len := v.tLen(0)
+		v.release()
 		if t0len < 2 {
 			t.Errorf("level-0 tables is less than 2, got %d", t0len)
 		}
@@ -653,8 +658,10 @@ func TestDb_GetEncountersEmptyLevel(t *testing.T) {
 			}
 			v := h.db.s.version()
 			if v.tLen(0) > 0 && v.tLen(2) > 0 {
+				v.release()
 				break
 			}
+			v.release()
 			h.put("a", "begin")
 			h.put("z", "end")
 			h.compactMem()
@@ -682,6 +689,7 @@ func TestDb_GetEncountersEmptyLevel(t *testing.T) {
 		if v.tLen(0) > 0 {
 			t.Errorf("level-0 tables more than 0, got %d", v.tLen(0))
 		}
+		v.release()
 
 		h.getVal("a", "begin")
 		h.getVal("z", "end")
@@ -833,9 +841,11 @@ func TestDb_RecoverWithLargeJournal(t *testing.T) {
 	h.getVal("big2", strings.Repeat("2", 200000))
 	h.getVal("small3", strings.Repeat("3", 10))
 	h.getVal("small4", strings.Repeat("4", 10))
-	if h.db.s.version().tLen(0) <= 1 {
+	v := h.db.s.version()
+	if v.tLen(0) <= 1 {
 		t.Errorf("tables-0 less than one")
 	}
+	v.release()
 
 	h.close()
 }
@@ -850,6 +860,7 @@ func TestDb_CompactionsGenerateMultipleFiles(t *testing.T) {
 	if v.tLen(0) > 0 {
 		t.Errorf("level-0 tables more than 0, got %d", v.tLen(0))
 	}
+	v.release()
 
 	n := 80
 
@@ -869,6 +880,7 @@ func TestDb_CompactionsGenerateMultipleFiles(t *testing.T) {
 	if v.tLen(1) <= 1 {
 		t.Errorf("level-1 tables less than 1, got %d", v.tLen(1))
 	}
+	v.release()
 
 	for i := 0; i < n; i++ {
 		h.getVal(numKey(i), strings.Repeat(fmt.Sprintf("v%09d", i), 100000/10))
@@ -993,6 +1005,7 @@ func TestDb_ApproximateSizes(t *testing.T) {
 		if v.tLen(1) == 0 {
 			t.Error("level-1 tables was zero")
 		}
+		v.release()
 	}
 
 	h.close()
@@ -1073,7 +1086,9 @@ func TestDb_HiddenValuesAreRemoved(t *testing.T) {
 		h.put("foo", "v1")
 		h.compactMem()
 		m := kMaxMemCompactLevel
-		num := s.version().tLen(m)
+		v := s.version()
+		num := v.tLen(m)
+		v.release()
 		if num != 1 {
 			t.Errorf("invalid level-%d len, want=1 got=%d", m, num)
 		}
@@ -1082,13 +1097,14 @@ func TestDb_HiddenValuesAreRemoved(t *testing.T) {
 		h.put("a", "begin")
 		h.put("z", "end")
 		h.compactMem()
-		v := s.version()
+		v = s.version()
 		if v.tLen(m) != 1 {
 			t.Errorf("invalid level-%d len, want=1 got=%d", m, v.tLen(m))
 		}
 		if v.tLen(m-1) != 1 {
 			t.Errorf("invalid level-%d len, want=1 got=%d", m-1, v.tLen(m-1))
 		}
+		v.release()
 
 		h.delete("foo")
 		h.put("foo", "v2")
@@ -1113,7 +1129,9 @@ func TestDb_DeletionMarkers2(t *testing.T) {
 	h.put("foo", "v1")
 	h.compactMem()
 	m := kMaxMemCompactLevel
-	num := s.version().tLen(m)
+	v := s.version()
+	num := v.tLen(m)
+	v.release()
 	if num != 1 {
 		t.Errorf("invalid level-%d len, want=1 got=%d", m, num)
 	}
@@ -1122,13 +1140,14 @@ func TestDb_DeletionMarkers2(t *testing.T) {
 	h.put("a", "begin")
 	h.put("z", "end")
 	h.compactMem()
-	v := s.version()
+	v = s.version()
 	if v.tLen(m) != 1 {
 		t.Errorf("invalid level-%d len, want=1 got=%d", m, v.tLen(m))
 	}
 	if v.tLen(m-1) != 1 {
 		t.Errorf("invalid level-%d len, want=1 got=%d", m-1, v.tLen(m-1))
 	}
+	v.release()
 
 	h.delete("foo")
 	h.allEntriesFor("foo", "[ DEL, v1 ]")
@@ -1249,9 +1268,11 @@ func TestDb_ManifestWriteError(t *testing.T) {
 		// Mem compaction (will succeed)
 		h.compactMem()
 		h.getVal("foo", "bar")
-		if n := h.db.s.version().tLen(kMaxMemCompactLevel); n != 1 {
+		v := h.db.s.version()
+		if n := v.tLen(kMaxMemCompactLevel); n != 1 {
 			t.Errorf("invalid total tables, want=1 got=%d", n)
 		}
+		v.release()
 
 		if i == 0 {
 			h.stor.SetWriteErr(storage.TypeManifest)

leveldb/db_util.go
@@ -42,7 +42,7 @@ func (p Sizes) Sum() (n uint64) {
 func (d *DB) cleanFiles() {
 	s := d.s
 
-	v := s.version()
+	v := s.version_NB()
 	tables := make(map[uint64]struct{})
 	for _, tt := range v.tables {
 		for _, t := range tt {

leveldb/session.go
@@ -8,8 +8,8 @@ package leveldb
 
 import (
 	"os"
+	"sync"
 	"sync/atomic"
-	"unsafe"
 
 	"github.com/syndtr/goleveldb/leveldb/errors"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
@@ -34,7 +34,8 @@ type session struct {
 	manifest *journalWriter
 
 	stCPtrs   [kNumLevels]iKey // compact pointers; need external synchronization
-	stVersion unsafe.Pointer   // current version
+	stVersion *version         // current version
+	vmu       sync.Mutex
 }
 
 func openSession(stor storage.Storage, o *opt.Options) (s *session, err error) {

leveldb/session_util.go
@@ -9,7 +9,6 @@ package leveldb
 import (
 	"fmt"
 	"sync/atomic"
-	"unsafe"
 
 	"github.com/syndtr/goleveldb/leveldb/journal"
 	"github.com/syndtr/goleveldb/leveldb/storage"
@@ -49,25 +48,28 @@ func (s *session) getFiles(t storage.FileType) []storage.File {
 
 // Get current version.
 func (s *session) version() *version {
-	return (*version)(atomic.LoadPointer(&s.stVersion))
+	s.vmu.Lock()
+	defer s.vmu.Unlock()
+	s.stVersion.ref++
+	return s.stVersion
 }
 
 // Get current version; no barrier.
 func (s *session) version_NB() *version {
-	return (*version)(s.stVersion)
+	return s.stVersion
 }
 
 // Set current version to v.
 func (s *session) setVersion(v *version) {
-	for {
-		old := s.stVersion
-		if atomic.CompareAndSwapPointer(&s.stVersion, old, unsafe.Pointer(v)) {
-			if old != nil {
-				(*version)(old).drop(v)
-			}
-			break
-		}
+	s.vmu.Lock()
+	v.ref = 1
+	if old := s.stVersion; old != nil {
+		v.ref++
+		old.next = v
+		old.release_NB()
 	}
+	s.stVersion = v
+	s.vmu.Unlock()
 }
 
 // Get current unused file number.

commit 291c883c71d5d4d421f6ab2acf6a330e8a9d5dd5

leveldb: version: Fix missing v.release() call on DB.flush()
This fixes regression introduced by 960ff1f37b7bf3d9f83e41ea2b949ca043652d49.

leveldb/db_write.go
@@ -49,6 +49,7 @@ func (d *DB) flush() (m *memdb.DB, err error) {
 			time.Sleep(time.Millisecond)
 		case mem.cur.Size() <= s.o.GetWriteBuffer():
 			// still room
+			v.release()
 			return mem.cur, nil
 		case mem.froze != nil:
 			if cwait {