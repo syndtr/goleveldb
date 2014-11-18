d8d1d2a5cc2d34c950dffa2f554525415d59f737
Suryandaru Triandana <syndtr@gmail.com>
2014-11-18 11:02:45 +0700

    table: uses forked snappy-go on github (closes #89)

--- leveldb/table/reader.go
+++ leveldb/table/reader.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 	"sync"
 
-	"code.google.com/p/snappy-go/snappy"
+	"github.com/syndtr/gosnappy/snappy"
 
 	"github.com/syndtr/goleveldb/leveldb/cache"
 	"github.com/syndtr/goleveldb/leveldb/comparer"
