87a8c51f3e07f88803ceb6f39a0663d974903a6e
Joshua Blum <jblum18@gmail.com>
2021-01-31 00:25:03 -0500

    unlock write

--- leveldb/db_write.go
+++ leveldb/db_write.go
@@ -247,6 +247,7 @@ func (db *DB) writeLocked(batch, ourBatch *Batch, merge, sync bool) error {
 	// Rotate memdb if it's reach the threshold.
 	if batch.internalLen >= mdbFree {
 		if _, err := db.rotateMem(0, false); err != nil {
+			db.unlockWrite(overflow, merged, err)
 			return err
 		}
 	}
